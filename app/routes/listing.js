5449274a080916200766284281873d8538585b46
Anand Angalig and Ian Baum <anand@>
2016-10-31 13:31:06 -0400

    add delete component for listings

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('listing', params.listing_id); //talking to Firabase to find the specific 'rental' given the params.rental_id, then passing it to  child components.
+  },
+
+  actions: {
+    destroyListing(listing) { 
+      listing.destroyRecord();
+      this.transitionTo('index');
+    },
+  }
 });
